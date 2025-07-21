98814c38b9a3453bed17c92637109fc7dbd4376d
bryndioleksandr <s.v.bryndo@gmail.com>
2025-07-21 13:53:30 +0300

    refactor: make almost all website in warm colors; now trying todo responsive

--- frontend/chimneys-web/app/layout.tsx
+++ frontend/chimneys-web/app/layout.tsx
@@ -36,7 +36,7 @@ const styles = {
         height: "100%",
         margin: "0",
         padding: "0",
-        backgroundColor: "#F8F9FA",
+        backgroundColor: "#FFF7F0",
     },
     contentWrapper: {
         width: "100%",
