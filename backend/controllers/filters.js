41f69d1fbdafed2ac3ba8199a95fc9680f3f3995
bryndioleksandr <s.v.bryndo@gmail.com>
2025-08-13 15:34:01 +0300

    chore: fix little error

--- backend/controllers/filters.js
+++ backend/controllers/filters.js
@@ -1,4 +1,4 @@
-import Product from '../models/Product.js';
+import Product from '../models/product.js';
 
 export const getFiltersByCategory = async (req, res) => {
     const { categoryId, subCategoryId, subSubCategoryId } = req.params;
