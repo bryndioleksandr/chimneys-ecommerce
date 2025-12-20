2e04606b86c454001634d92c8156b42687a3e401
bryndioleksandr <s.v.bryndo@gmail.com>
2025-12-20 18:51:40 +0200

    feat: swap gmail smtp on resend.com

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,7 +1,35 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import { Resend } from 'resend';
 
 dotenv.config();
+
+const resend = new Resend(process.env.RESEND_URL);
+
+export const sendInfoEmailResend = async (to, subject, htmlContent) => {
+    try {
+        let sender = 'onboarding@resend.dev'; // temp (without domen)
+        let recipient = to;
+
+        if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
+            console.log(`Development mode: redirecting email from ${to} to my email`);
+            recipient = 's.v.bryndo@gmail.com';
+        } else {
+            sender = 'Магазин Димарів <orders@dymohit.com.ua>';
+        }
+
+        const data = await resend.emails.send({
+            from: sender,
+            to: recipient,
+            subject: subject,
+            html: htmlContent,
+        });
+
+        console.log('Email sent:', data);
+    } catch (error) {
+        console.error('Resend error:', error);
+    }
+};
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -22,13 +50,17 @@ export const sendVerificationEmail = async (to, code) => {
     console.log('after email send');
 };
 
-export const sendInfoEmail = async (to, subject, text) => {
+export const sendInfoEmail = async ({to, subject, text}) => {
     console.log('trying to send an email');
-    await transporter.sendMail({
-        from: `"Chimney Store" <${process.env.EMAIL_FROM}>`,
-        to,
-        subject,
-        html: `<p>${text}</p>`
-    });
-    console.log('after email send');
+    try {
+        await transporter.sendMail({
+            from: `"Chimney Store" <${process.env.EMAIL_FROM}>`,
+            to,
+            subject,
+            html: `<p>${text}</p>`
+        });
+        console.log('after email send');
+    } catch (error) {
+        console.error('Email sending error:', error);
+    }
 };
